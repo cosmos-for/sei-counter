626279d23cb0c639f2a4941ec9882e8d8b2568e6
lshoo <lshoo36@gmail.com>
2023-06-06 17:15:05 +0800

    init project

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use sei_counter::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
